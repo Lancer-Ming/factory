28468a7e65b6a01f20f84b2d7f39bea2069bb010
pinkqy <694325192@qq.com>
2018-07-12 09:02:10 +0800

    x

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
 {{--<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">--}}
 <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
@@ -113,6 +113,6 @@
 </div>
 {{--<script src="{{ mix('js/manifest.js') }}"></script>--}}
 {{--<script src="{{ mix('js/vendor.js') }}"></script>--}}
-<script src="{{ mix('js/app.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
