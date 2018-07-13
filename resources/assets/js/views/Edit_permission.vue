9d57d1949d61dbb3afa2ed7e695d82f3dad6a126
lin <1844826738@qq.com>
2018-07-13 09:18:25 +0800

    用户管理编辑权限

--- resources/assets/js/views/Edit_permission.vue
+++ resources/assets/js/views/Edit_permission.vue
@@ -2,27 +2,29 @@
     <div class="container" style="border: 1px solid #eee">
         <div class="edit-nav" v-for="(header,index) in edit_header">
             <div class="edit_firstmenu">
-                <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange" :key="index">{{header.label}}</el-checkbox>
+                <el-checkbox :indeterminate="isIndeterminate" v-model="checkedFirst" @change="handleCheckAllChange" :key="index">{{header.label}}</el-checkbox>
             </div>
             <div class="edit_box clearfix" v-for="(item,index) in header.children">
-                <div class="edit-secondmenu">
-                    <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange" >
-                        <el-checkbox :key="index">{{item.label}}</el-checkbox>
+                <div class="edit-secondmenu clearfix">
+                    <el-checkbox-group v-model="checkedSecond" @change="handleCheckedSecondChange">
+                        <el-checkbox :key="index" style="color: #3a5aaa;">{{item.label}}</el-checkbox>
                     </el-checkbox-group>
                 </div>
-                <div class="edit-threemenu">
-                    <el-checkbox-group v-model="checkedOpeartions" @change="handleCheckedOpeartionsChange">
-                        <el-checkbox v-for="(child,child_index) in item.children" :key="child_index">{{child.label}}</el-checkbox>
-                    </el-checkbox-group>
-                    <!--<div class="edit-fourmenu">-->
-                        <!--<el-checkbox-group v-model="checkedOpeartions" @change="handleCheckedOpeartionsChange" v-if="item.children.length > 0">-->
-                            <!--<el-checkbox v-for="(children,children_index) in child.children" :key="children_index">{{children.label}}</el-checkbox>-->
-                        <!--</el-checkbox-group>-->
-                    <!--</div>-->
-                </div>
+                <div class="edit_box2" v-for="(child,child_index) in item.children">
+                    <div class="edit-threemenu clearfix">
+                        <el-checkbox-group v-model="checkedThree" @change="handleCheckedThreeChange">
+                            <el-checkbox :key="child_index" style="color: #7f47a3;">{{child.label}}</el-checkbox>
+                        </el-checkbox-group>
 
+                    </div>
+                    <div class="edit-fourmenu" v-if="child.children.length > 0">
+                        <el-checkbox-group v-model="checkedFour" @change="handleCheckedFourChange">
+                            <el-checkbox v-for="(children,children_index) in child.children" :key="children_index" style="color: #aa5f3a;">{{children.label}}</el-checkbox>
+                        </el-checkbox-group>
+                    </div>
+                </div>
             </div>
-    </div>
+        </div>
     </div>
 </template>
 
@@ -32,10 +34,11 @@
         name: 'edit_permission',
         data() {
             return {
-                edit_header:[],
-                checkAll: false,
-                checkedCities: ['建筑工人库', '管理人员库'],
-                checkedOpeartions:['删除','修改'],
+                edit_header: [],
+                checkedFirst: false,
+                checkedSecond: [],
+                checkedThree: [],
+                checkedFour: [],
                 isIndeterminate: true
             }
         },
@@ -46,61 +49,74 @@
                 console.log(this.edit_header)
             })
 
-                },
+        },
         methods: {
             handleCheckAllChange(val) {
-                this.checkedCities = val ? edit_header : []
-                this.checkedOpeartions = val ? edit_header : []
+                this.checkedSecond = val ? this.edit_header : []
                 this.isIndeterminate = false
             },
-            handleCheckedCitiesChange(value) {
+            handleCheckedSecondChange(value) {
                 let checkedCount = value.length
-                this.checkAll = checkedCount === this.edit_header.length
+                this.checkedFirst = checkedCount === this.edit_header.length
                 this.isIndeterminate = checkedCount > 0 && checkedCount < this.edit_header.length
             },
-            handleCheckedOpeartionsChange(value) {
+            handleCheckedThreeChange(value) {
                 let checkedOount = value.length
-                this.checkAll = checkedOount === this.edit_header.length
+                this.checkedFirst = checkedOount === this.edit_header.length
                 this.isIndeterminate = checkedOount > 0 && checkedOount < this.edit_header.length
+            },
+            handleCheckedFourChange(value){
+                let checkedOount = value.length
+
             }
         }
     }
 </script>
 <style>
-    edit-nav{
+    edit-nav {
         width: 100%;
         margin: 10px 0px;
-        border:1px solid #eee;
+        border: 1px solid #eee;
     }
-    .edit_firstmenu{
-        width:100%;
-        padding:5px 10px;
-        border-bottom:1px solid #eee;
+
+    .edit_firstmenu {
+        width: 100%;
+        padding: 5px 10px;
+        border-bottom: 1px solid #eee;
     }
-    .edit_box{
+
+    .edit_box {
         width: 100%;
         margin: 10px auto;
     }
+
     .edit-secondmenu{
-        width:20%;
+        width: 20%;
         float: left;
     }
-    .edit-secondmenu label{
-        display: block;
-        padding:0px 30px;
+    .edit_box2{
+        width: 80%;
+        margin-left: 20%;
     }
-    .edit-secondmenu label.el-checkbox+.el-checkbox{
+    .edit-secondmenu label {
+        display: block;
+        padding: 0px 30px;
+    }
+
+    .edit-secondmenu label.el-checkbox + .el-checkbox {
         margin-left: 0px;
     }
+
     .edit-threemenu{
-        width: 70%;
         float: left;
     }
-    /*.edit-fourmenu{*/
-        /*width: 25%;*/
-        /*float: left;*/
-    /*}*/
-    .clearfix:after{
+    .edit-fourmenu{
+        margin-left:50%;
+    }
+    .edit-threemenu .el-checkbox__label {
+        padding-right: 15px;
+    }
+    .clearfix:after {
         display: block;
         visibility: hidden;
         font-size: 0;
@@ -108,7 +124,8 @@
         content: '';
         clear: both;
     }
-    .clearfix{
+
+    .clearfix {
         zoom: 1;
     }
 </style>
