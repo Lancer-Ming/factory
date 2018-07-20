84a79be389ec121a161f36f44bd9057a20c3689c
lin <1844826738@qq.com>
2018-07-20 16:13:32 +0800

    省市级加v-model

--- resources/assets/js/views/Project.vue
+++ resources/assets/js/views/Project.vue
@@ -60,7 +60,7 @@
                     <el-input auto-complete="off"></el-input>
                 </el-form-item>
                 <el-form-item label="所在省市区" :label-width="formLabelWidth">
-                    <el-cascader :options="citys"></el-cascader>
+                    <el-cascader v-model="address" :options="citys"></el-cascader>
                 </el-form-item>
                 <el-form-item label="详细地址" :label-width="formLabelWidth">
                     <el-input auto-complete="off"></el-input>
@@ -82,6 +82,7 @@
                 </el-form-item>
                 <el-form-item label="GPS" :label-width="formLabelWidth">
                     <el-input auto-complete="off"></el-input>
+                    <el-button type="info" disabled>信息按钮</el-button>
                 </el-form-item>
                 <el-form-item label="接收时间" :label-width="formLabelWidth">
                     <el-date-picker v-model="value1" type="date" placeholder="接收时间">
@@ -113,6 +114,7 @@
                 addform: false,
                 formLabelWidth: "100px",
                 num1: 1,
+                address:[],
                 //建设方式
                 buildType: [],
                 //资金来源
@@ -298,9 +300,132 @@
         width: 50%;
     }
     .el-input{
-        width: 80%;
+        width: 70%;
     }
     .el-input.el-input--suffix{
         width: 100%;
     }
 </style>
+
+
+
+<!--<template>-->
+    <!--<div style="padding-top:50px; border:1px solid red">-->
+        <!--<Modal @on-cancel="cancel" v-model="showMapComponent" width="800" :closable="false" :mask-closable="false">-->
+            <!--<baidu-map v-bind:style="mapStyle" class="bm-view" ak="你的密钥"-->
+                    <!--:center="center"-->
+                    <!--:zoom="zoom"-->
+                    <!--:scroll-wheel-zoom="true"-->
+                    <!--@click="getClickInfo"-->
+                    <!--@moving="syncCenterAndZoom"-->
+                    <!--@moveend="syncCenterAndZoom"-->
+                    <!--@zoomend="syncCenterAndZoom">-->
+                <!--<bm-view style="width: 100%; height:500px;"></bm-view>-->
+                <!--<bm-marker :position="{lng: center.lng, lat: center.lat}" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">-->
+                <!--</bm-marker>-->
+                <!--<bm-control :offset="{width: '10px', height: '10px'}">-->
+                    <!--<bm-auto-complete v-model="keyword" :sugStyle="{zIndex: 999999}">-->
+                        <!--<input type="text" placeholder="请输入搜索关键字" class="serachinput">-->
+                    <!--</bm-auto-complete>-->
+                <!--</bm-control>-->
+                <!--<bm-local-search :keyword="keyword" :auto-viewport="true" style="width:0px;height:0px;overflow: hidden;"></bm-local-search>-->
+            <!--</baidu-map>-->
+            <!--<div slot="footer" style="margin-top: 0px;">-->
+                <!--<Button @click="cancel" type="ghost"-->
+                        <!--style="width: 60px;height: 36px;">取消-->
+                <!--</Button>-->
+                <!--<Button type="primary" style="width: 60px;height: 36px;" @click="confirm">确定</Button>-->
+            <!--</div>-->
+        <!--</Modal>-->
+    <!--</div>-->
+<!--</template>-->
+
+<!--<script>-->
+
+    <!--import {BaiduMap, BmControl, BmView, BmAutoComplete, BmLocalSearch, BmMarker} from 'vue-baidu-map'-->
+
+    <!--export default {-->
+        <!--components: {-->
+            <!--BaiduMap,-->
+            <!--BmControl,-->
+            <!--BmView,-->
+            <!--BmAutoComplete,-->
+            <!--BmLocalSearch,-->
+            <!--BmMarker-->
+        <!--},-->
+        <!--data: function () {-->
+            <!--return {-->
+                <!--showMapComponent: this.value,-->
+                <!--keyword: '',-->
+                <!--mapStyle: {-->
+                    <!--width: '100%',-->
+                    <!--height: this.mapHeight + 'px'-->
+                <!--},-->
+                <!--center: {lng: 116.404, lat: 39.915},-->
+                <!--zoom: 15-->
+            <!--}-->
+        <!--},-->
+        <!--watch: {-->
+            <!--value: function (currentValue) {-->
+                <!--this.showMapComponent = currentValue-->
+                <!--if (currentValue) {-->
+                    <!--this.keyword = ''-->
+                <!--}-->
+            <!--}-->
+        <!--},-->
+        <!--props: {-->
+            <!--value: Boolean,-->
+            <!--mapHeight: {-->
+                <!--type: Number,-->
+                <!--default: 500-->
+            <!--}-->
+        <!--},-->
+        <!--methods: {-->
+            <!--/***-->
+             <!--* 地图点击事件。-->
+             <!--*/-->
+            <!--getClickInfo (e) {-->
+                <!--this.center.lng = e.point.lng-->
+                <!--this.center.lat = e.point.lat-->
+            <!--},-->
+            <!--syncCenterAndZoom (e) {-->
+                <!--const {lng, lat} = e.target.getCenter()-->
+                <!--this.center.lng = lng-->
+                <!--this.center.lat = lat-->
+                <!--this.zoom = e.target.getZoom()-->
+            <!--},-->
+            <!--/***-->
+             <!--* 确认-->
+             <!--*/-->
+            <!--confirm: function () {-->
+                <!--this.showMapComponent = false-->
+                <!--this.$emit('map-confirm', this.center)-->
+            <!--},-->
+            <!--/***-->
+             <!--* 取消-->
+             <!--*/-->
+            <!--cancel: function () {-->
+                <!--this.showMapComponent = false-->
+                <!--this.$emit('cancel', this.showMapComponent)-->
+            <!--}-->
+        <!--}-->
+    <!--}-->
+<!--</script>-->
+
+<!--<style scoped>-->
+    <!--.serachinput{-->
+        <!--width: 300px;-->
+        <!--box-sizing: border-box;-->
+        <!--padding: 9px;-->
+        <!--border: 1px solid #dddee1;-->
+        <!--line-height: 20px;-->
+        <!--font-size: 16px;-->
+        <!--height: 38px;-->
+        <!--color: #333;-->
+        <!--position: relative;-->
+        <!--border-radius: 4px;-->
+        <!-- -webkit-box-shadow: #666 0px 0px 10px;-->
+        <!-- -moz-box-shadow: #666 0px 0px 10px;-->
+        <!--box-shadow: #666 0px 0px 10px;-->
+    <!--}-->
+<!--</style>-->
