1d02c7a995a56b7620b45b93b00ecf4fbc732eee
Lancer_cm <675983959@qq.com>
2018-08-01 14:50:33 +0800

    单位搜索和项目搜索

--- app/Http/Controllers/Item/ProjectsController.php
+++ app/Http/Controllers/Item/ProjectsController.php
@@ -13,27 +13,23 @@ class ProjectsController extends Controller
 {
     public function index(Request $request)
     {
-        $where = function($query) use ($request) {
-            if($request->has('name') && trim($request->name) !== '') {
-                $query->where('name', 'like', '%'.$request->name.'%');
+        $where = function ($query) use ($request) {
+            if ($request->has('name') && trim($request->name) !== '') {
+                $query->where('name', 'like', '%' . $request->name . '%');
             }
         };
 
-        $items = Item::where($where)->with([
-            'units' => function($query) {
-                $query->orderBy('created_at', 'desc');
-            }
-        ])->orderBy('created_at', 'desc')->get()->toArray();
+        $items = Item::where($where)->with(['units' => function ($query) {
+            $query->orderBy('created_at', 'desc');
+        }])->orderBy('created_at', 'desc')->get()->toArray();
 
-        $unit_ids = Unit::where($where)->pluck('id');
-        $item_ids = \DB::table('item_units')->whereIn('contract_id', $unit_ids)->pluck('item_id');
-        $items2 = Item::whereIn('id', $item_ids)->with([
-            'units' => function($query) {
+        if ($request->has('name') && trim($request->name) !== '') {
+            $unit_ids = Unit::where($where)->pluck('id');
+            $item_ids = \DB::table('item_units')->whereIn('contract_id', $unit_ids)->pluck('item_id');
+            $items2 = Item::whereIn('id', $item_ids)->with(['units' => function ($query) {
                 $query->orderBy('created_at', 'desc');
-            }
-        ])->orderBy('created_at', 'desc')->get()->toArray();
+            }])->orderBy('created_at', 'desc')->get()->toArray();
 
-        if (count($items2) > 0) {
             $result = array_unique(array_merge($items, $items2));
             return successJson($result);
         }
@@ -43,41 +39,9 @@ class ProjectsController extends Controller
 
     public function store(ItemRequest $request)
     {
-        $item = Item::create([
-            'item_category_id' => $request->item_category_id,
-            'invest_id' => $request->invest_id,
-            'build_type_id' => $request->build_type_id,
-            'structural_type_id' => $request->structural_type_id,
-            'item_no' => $request->item_no,
-            'project_no' => $request->project_no,
-            'country' => $request->country,
-            'province' => $request->province,
-            'city' => $request->city,
-            'county' => $request->county,
-            'detail' => $request->detail,
-            'permit_no' => $request->permit_no,
-            'area' => $request->area,
-            'total_amount' => $request->total_amount,
-            'chargeman' => $request->chargeman,
-            'chargeman_tel' => $request->chargeman_tel,
-            'gps' => $request->gps,
-            'received_at' => $request->received_at,
-            'started_at' => $request->started_at,
-            'ended_at' => $request->ended_at,
-            'started_at' => $request->started_at,
-            'name' => $request->name,
-        ]);
+        $item = Item::create(['item_category_id' => $request->item_category_id, 'invest_id' => $request->invest_id, 'build_type_id' => $request->build_type_id, 'structural_type_id' => $request->structural_type_id, 'item_no' => $request->item_no, 'project_no' => $request->project_no, 'country' => $request->country, 'province' => $request->province, 'city' => $request->city, 'county' => $request->county, 'detail' => $request->detail, 'permit_no' => $request->permit_no, 'area' => $request->area, 'total_amount' => $request->total_amount, 'chargeman' => $request->chargeman, 'chargeman_tel' => $request->chargeman_tel, 'gps' => $request->gps, 'received_at' => $request->received_at, 'started_at' => $request->started_at, 'ended_at' => $request->ended_at, 'started_at' => $request->started_at, 'name' => $request->name,]);
 
-        ItemUnit::insert([
-            'item_id' => $item->id,
-            'contract_id' => $request->contract_id,
-            'subcontract_id' => $request->subcontract_id,
-            'supervisor_id' => $request->supervisor_id,
-            'servey_id' => $request->servey_id,
-            'design_id' => $request->design_id,
-            'trail_id' => $request->trail_id,
-            'safety_station_id' => $request->safety_station_id,
-        ]);
+        ItemUnit::insert(['item_id' => $item->id, 'contract_id' => $request->contract_id, 'subcontract_id' => $request->subcontract_id, 'supervisor_id' => $request->supervisor_id, 'servey_id' => $request->servey_id, 'design_id' => $request->design_id, 'trail_id' => $request->trail_id, 'safety_station_id' => $request->safety_station_id,]);
 
         $items = Item::getItems();
 
@@ -89,57 +53,16 @@ class ProjectsController extends Controller
         $data = $item;
         $units = Unit::get();
         $item_units = $item->itemUnit;
-        $data['item_unit'] = [
-            'contract_id' => $units->firstWhere('id', $item_units->contract_id)->name,
-            'subcontract_id' => $units->firstWhere('id', $item_units->subcontract_id)->name,
-            'build_id' => $units->firstWhere('id', $item_units->build_id)->name,
-            'supervisor_id' => $units->firstWhere('id', $item_units->supervisor_id)->name,
-            'servey_id' => $units->firstWhere('id', $item_units->servey_id)->name,
-            'design_id' => $units->firstWhere('id', $item_units->design_id)->name,
-            'trail_id' => $units->firstWhere('id', $item_units->trail_id)->name,
-            'safety_station_id' => $units->firstWhere('id', $item_units->safety_station_id)->name
-        ];
+        $data['item_unit'] = ['contract_id' => $units->firstWhere('id', $item_units->contract_id)->name, 'subcontract_id' => $units->firstWhere('id', $item_units->subcontract_id)->name, 'build_id' => $units->firstWhere('id', $item_units->build_id)->name, 'supervisor_id' => $units->firstWhere('id', $item_units->supervisor_id)->name, 'servey_id' => $units->firstWhere('id', $item_units->servey_id)->name, 'design_id' => $units->firstWhere('id', $item_units->design_id)->name, 'trail_id' => $units->firstWhere('id', $item_units->trail_id)->name, 'safety_station_id' => $units->firstWhere('id', $item_units->safety_station_id)->name];
 
         return successJson($data);
     }
 
     public function update(ItemRequest $request, Item $project)
     {
-        $project->update([
-            'item_category_id' => $request->item_category_id,
-            'invest_id' => $request->invest_id,
-            'build_type_id' => $request->build_type_id,
-            'structural_type_id' => $request->structural_type_id,
-            'item_no' => $request->item_no,
-            'project_no' => $request->project_no,
-            'country' => $request->country,
-            'province' => $request->province,
-            'city' => $request->city,
-            'county' => $request->county,
-            'detail' => $request->detail,
-            'permit_no' => $request->permit_no,
-            'area' => $request->area,
-            'total_amount' => $request->total_amount,
-            'chargeman' => $request->chargeman,
-            'chargeman_tel' => $request->chargeman_tel,
-            'gps' => $request->gps,
-            'received_at' => $request->received_at,
-            'started_at' => $request->started_at,
-            'ended_at' => $request->ended_at,
-            'started_at' => $request->started_at,
-            'name' => $request->name,
-        ]);
+        $project->update(['item_category_id' => $request->item_category_id, 'invest_id' => $request->invest_id, 'build_type_id' => $request->build_type_id, 'structural_type_id' => $request->structural_type_id, 'item_no' => $request->item_no, 'project_no' => $request->project_no, 'country' => $request->country, 'province' => $request->province, 'city' => $request->city, 'county' => $request->county, 'detail' => $request->detail, 'permit_no' => $request->permit_no, 'area' => $request->area, 'total_amount' => $request->total_amount, 'chargeman' => $request->chargeman, 'chargeman_tel' => $request->chargeman_tel, 'gps' => $request->gps, 'received_at' => $request->received_at, 'started_at' => $request->started_at, 'ended_at' => $request->ended_at, 'started_at' => $request->started_at, 'name' => $request->name,]);
 
-        ItemUnit::where('item_id', $project->id)->update([
-            'contract_id' => $request->contract_id,
-            'subcontract_id' => $request->subcontract_id,
-            'supervisor_id' => $request->supervisor_id,
-            'servey_id' => $request->servey_id,
-            'design_id' => $request->design_id,
-            'trail_id' => $request->trail_id,
-            'build_id' => $request->build_id,
-            'safety_station_id' => $request->safety_station_id,
-        ]);
+        ItemUnit::where('item_id', $project->id)->update(['contract_id' => $request->contract_id, 'subcontract_id' => $request->subcontract_id, 'supervisor_id' => $request->supervisor_id, 'servey_id' => $request->servey_id, 'design_id' => $request->design_id, 'trail_id' => $request->trail_id, 'build_id' => $request->build_id, 'safety_station_id' => $request->safety_station_id,]);
 
         $items = Item::getItems();
         return successJson($items, '操作成功');
@@ -159,7 +82,7 @@ class ProjectsController extends Controller
     {
         if ($request->has('id') && count($request->id) > 0) {
             $units = Unit::whereIn('id', $request->id)->select('id', 'name')->get();
-            foreach($units as $key => $unit) {
+            foreach ($units as $key => $unit) {
                 $units[$key]['label'] = $unit['name'];
                 unset($units[$key]['name']);
                 $units[$key]['value'] = $unit['id'];
