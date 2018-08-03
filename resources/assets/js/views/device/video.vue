7877cdcdfa3fd1ba012fd1b6c3479c72833b3d65
lin <1844826738@qq.com>
2018-08-03 09:44:29 +0800

    取消按钮

--- resources/assets/js/views/device/video.vue
+++ resources/assets/js/views/device/video.vue
@@ -151,7 +151,7 @@
                 </p>
             </div>
             <div slot="footer" class="dialog-footer" style="text-align: center;">
-                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button @click="addCamera = false">取 消</el-button>
                 <el-button type="warning" @click="confirm">确 定</el-button>
             </div>
         </el-dialog>
