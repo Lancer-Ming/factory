9ef45d65a687b4f39150bd2bba8cf19fb0d3440c
lin <1844826738@qq.com>
2018-08-13 18:02:08 +0800

    塔机页面布局

--- resources/assets/js/views/device/crane.vue
+++ resources/assets/js/views/device/crane.vue
@@ -1,142 +1,270 @@
 <template>
     <div class="container crane">
-        <split-pane v-on:resize="resize" split="vertical" :default-percent='20' :min-percent='10' class="projectBox" style="background: #fff;">
-            <template slot="paneL">
-                <div class="left-container">
-                    <div class="crane-tit">
-                        <el-row>
-                            <i class="el-icon-d-arrow-left tool" style="color: #8fbafe;float: right;"></i>
-                        </el-row>
-                    </div>
-                    <el-row class="crane-l-query">
-                        <el-col :span="2">&nbsp;</el-col>
-                        <el-col :span="11"><el-input size="mini" style="width: 100%;margin-top: 5px;"></el-input></el-col>
-                        <el-col :span="2">&nbsp;</el-col>
-                        <el-col :span="9" class="query-text"><el-button type="warning" icon="el-icon-search" plain size="mini">查询</el-button></el-col>
-                    </el-row>
+        <el-row style="margin-top: 20px;">
+            <el-button type="primary"  icon="el-icon-plus" size="mini" style="border-radius: 8px;" @click="handleAdd">新增</el-button>
+            <el-button type="primary" plain icon="el-icon-edit" size="mini" style="border-radius: 8px;">编辑</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" style="border-radius: 8px;">删除</el-button>
+            <el-button type="warning" plain icon="el-icon-download" size="mini" style="border-radius: 8px;">下载</el-button>
+        </el-row>
+        <el-row style="margin-top: 20px;">
+            <el-form :model="form" size="mini" class="crane-head">
+                <el-form-item label="项目名称" :label-width="formLabelW">
+                    <el-input  auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="产权单位" :label-width="formLabelW">
+                    <el-input  auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="备案编号" :label-width="formLabelW">
+                    <el-input  auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="黑匣子SN" :label-width="formLabelW">
+                    <el-input  auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="是否在线" :label-width="formLabelW">
+                    <el-select v-model="form.region" placeholder="请选择活动区域">
+                        <el-option label="是" value="shanghai"></el-option>
+                        <el-option label="否" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-button type="primary" plain size="mini" style="float: left">查询</el-button>
+                <el-button size="mini" style="float: left">清空</el-button>
+            </el-form>
+        </el-row>
+        <el-table class="crane-tab"
+                :data="tableData"
+                border
+                stripe
+                style="width: 100%;">
+            <el-table-column
+                    prop="id"
+                    align="center"
+                    width="60"
+                    fixed
+                    sortable
+                    label="#"
+            >
+                <template slot-scope="scope">
+                    <span>{{ scope.row.id }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column
+                    fixed
+                    prop="SN"
+                    label="SN"
+                    width="100"
+                    align="center"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="number"
+                    label="备案编号"
+                    width="120"
+                    align="center"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="name"
+                    label="塔机名称"
+                    width="120"
+                    align="center"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="project"
+                    label="所在项目"
+                    width="300"
+                    align="center"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="tell"
+                    label="SIM卡号"
+                    width="200"
+                    align="center"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="remind"
+                    label="欠费提醒"
+                    width="300"
+                    align="center"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="data"
+                    label="最近上线时间"
+                    width="200"
+                    align="center"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="collision"
+                    label="防碰撞"
+                    width="60"
+                    align="center"
+            >
+                <template slot-scope="scope">
+                    <el-checkbox v-model="checked2" disabled></el-checkbox>
+                </template>
+            </el-table-column>
+            <el-table-column
+                    prop="online"
+                    label="是否在线"
+                    width="100"
+                    align="center"
+            >
+                <template slot-scope="scope">
+                    <el-checkbox v-model="checked1" disabled></el-checkbox>
+                </template>
+            </el-table-column>
+            <el-table-column
+                    prop="state"
+                    label="监控状态"
+                    width="120"
+                    align="center"
+            >
+            </el-table-column>
+            <el-table-column
+                    fixed="right"
+                    label="操作"
+                    width="200"
+                    align="center"
+            >
+                <template slot-scope="scope">
+                    <el-button @click="handleClick(scope.row)" type="primary" plain size="mini">远程断电</el-button>
+                    <el-button type="primary" plain size="mini">设置GPS</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-row>
+            <el-row style="margin-top: 20px;">
+                <el-pagination
+                        @size-change="handleSizeChange"
+                        @current-change="handleCurrentChange"
+                        :current-page="currentPage"
+                        :page-sizes="[100, 200, 300, 400]"
+                        :page-size="100"
+                        layout="total, sizes, prev, pager, next, jumper"
+                        :total="400"
+                        align="center"
+                >
+                </el-pagination>
+            </el-row>
+        </el-row>
 
-                </div>
-            </template>
-            <template slot="paneR">
-                <div class="right-container">
-                    <el-form ref="form" :model="form" label-width="120px" style="margin-top: 20px;">
-                        <el-form-item label="塔机名称">
-                            <el-input size="mini"></el-input>
-                        </el-form-item>
-                        <el-form-item label="塔机ID">
-                            <el-input size="mini"></el-input>
-                        </el-form-item>
-                        <el-form-item label="塔机状态">
-                            <el-select v-model="value" placeholder="请选择" size="mini">
-                                <el-option
-                                        v-for="item in options"
-                                        :key="item.value"
-                                        :label="item.label"
-                                        :value="item.value"
-                                >
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item label="司机姓名">
-                            <el-input size="mini"></el-input>
-                        </el-form-item>
-                        <el-form-item label="司机ID">
-                            <el-input size="mini"></el-input>
-                        </el-form-item>
-                        <el-form-item label="黑匣子编号">
-                            <el-input size="mini"></el-input>
-                        </el-form-item>
-                        <el-form-item label="监控厂商">
-                            <el-input size="mini"></el-input>
-                        </el-form-item>
-                        <el-form-item label="监控状态">
-                            <el-select v-model="value" placeholder="请选择" size="mini">
-                                <el-option
-                                        v-for="item in options1"
-                                        :key="item.value"
-                                        :label="item.label"
-                                        :value="item.value"
-                                >
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item label="IP">
-                            <el-input size="mini"></el-input>
-                        </el-form-item>
-                        <el-form-item label="端口">
-                            <el-input size="mini"></el-input>
-                        </el-form-item>
-                    </el-form>
-                </div>
-            </template>
-        </split-pane>
+
+        <el-dialog title="新增塔机信息" :visible.sync="craneAdd">
+            <el-form :model="form">
+                <el-form-item label="备案编号" :label-width="formLabelWidth">
+                    <el-input v-model="form.number" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="所在项目" :label-width="formLabelWidth">
+                    <el-select v-model="form.region" placeholder="请选择活动区域">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="楼号" :label-width="formLabelWidth">
+                    <el-input auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="产权单位" :label-width="formLabelWidth">
+                    <el-input auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="塔吊出产日期" :label-width="formLabelWidth">
+                    <el-input auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="塔吊型号" :label-width="formLabelWidth">
+                    <el-input auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="塔吊生产日期" :label-width="formLabelWidth">
+                    <el-input auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="塔吊生产厂商" :label-width="formLabelWidth">
+                    <el-input auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="塔吊出产编号" :label-width="formLabelWidth">
+                    <el-input auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="是否监控" :label-width="formLabelWidth">
+                    <el-switch
+                            v-model="value"
+                    >
+                    </el-switch>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button>取 消</el-button>
+                <el-button type="primary">确 定</el-button>
+            </div>
+        </el-dialog>
+
     </div>
 </template>
 
 <script>
-    import splitPane from 'vue-splitpane'
     export default {
-        components: {
-            splitPane,
-        },
         data() {
             return {
-                form: {
-
+                form:{
+                    name: ''
                 },
-                options: [
-                    {
-                    value: '选项1',
-                    label: '在线'
+                formLabelW: '75px',
+                formLabelWidth: '120px',
+                tableData: [{
+                    SN: 104666,
+                    name: '#1',
+                    project: '中铁隧道集团科技大厦',
+                    tell: 1064687974918,
+                    remind: '[续费或更换卡号]剩余9个月',
+                    data: '2018-08-13 13:00:46',
+                    state: '监控中',
                 },
                     {
-                    value: '选项2',
-                    label: '离线'
-                }],
-                value: '',
-                options1: [
-                    {
-                    value: '选项1',
-                    label: '停用'
-                    },
-                    {
-                        value: '选项2',
-                        label: '监控'
-                    }],
+                        SN: 104666,
+                        name: '#1',
+                        project: '中铁隧道集团科技大厦',
+                        tell: 1064687974918,
+                        remind: '[续费或更换卡号]剩余9个月',
+                        data: '2018-08-13 13:00:46',
+                        state: '监控中',
+                    }
+                ],
+                currentPage: 4,
+                checked1: false,
+                checked2: true,
+                craneAdd: false,
+                value: true,
             }
         },
         methods: {
-            resize() {
-                console.log('resize')
+            handleClick(row) {
+                console.log(row);
+            },
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
+            },
+            handleAdd(){
+                this.form = {
+                    name: ''
+                }
+                this.craneAdd = true
+                this.submitType = 'add'
             }
-        }
+
+        },
     }
 
 </script>
 
 <style>
-    .left-container{
-        height: 100%;
-        width: 100%;
-
-    }
-    .right-container{
-        height: 100%;
-    }
-    .crane-tit{
-        width:100%;
-        padding: 10px 5px;
-        background: #E0ECFF;
-    }
-    .crane-l-query{
-        width: 100%;
-        margin: 20px auto;
-        padding-bottom: 20px;
-        border-bottom: 1px solid #eee;
-    }
-    .right-container .el-form-item{
-        width: 33%;
+    .crane-head .el-form-item{
+        width: 30%;
         float: left;
+        padding-right: 60px;
     }
-
+    .crane-tab .el-table__row{
+        height: 35px;
+    }
 </style>
