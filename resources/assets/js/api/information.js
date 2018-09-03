24b9b43564747d713ec463ca925c27c965834623
lin <1844826738@qq.com>
2018-09-03 18:05:03 +0800

    扬尘监控查询

--- resources/assets/js/api/information.js
+++ resources/assets/js/api/information.js
@@ -1,10 +1,10 @@
 import axios from "../utils/request"
 
-export function getinformation(page,sn,pagesize){
+export function getinformation(page, sn, pagesize, time) {
     return axios({
         url: `/video/dust/warn/${sn}`,
         method: 'get',
-        params:{ page,pagesize }
+        params: {page, pagesize, time}
     })
 }
 
--- resources/assets/js/views/monitors/subDust/WorkingData.vue
+++ resources/assets/js/views/monitors/subDust/WorkingData.vue
@@ -5,9 +5,15 @@
                 <el-form ref="form" :model="form" inline size="mini">
                     <el-form-item label="时间">
                         <el-date-picker
-                                v-model="form.date"
-                                type="date"
-                                placeholder="选择日期">
+                                v-model="form.time"
+                                type="daterange"
+                                placeholder="选择日期" format="yyyy-MM-dd" value-format="yyyy-MM-dd"
+                                unlink-panels
+                                range-separator="至"
+                                start-placeholder="开始日期"
+                                end-placeholder="结束日期"
+                                :picker-options="pickerOptions2"
+                        >
                         </el-date-picker>
                     </el-form-item>
                     <el-form-item label="">
@@ -149,7 +155,7 @@
         data() {
             return {
                 form: {
-                    date: '',
+                    time: '',
                 },
                 tableData: [],
                 currentPage: 1, //当前页数
@@ -157,6 +163,35 @@
                 perPagesize: perPagesize,
                 total: null,
                 distribution: false,
+
+                pickerOptions2: {
+                    shortcuts: [{
+                        text: '最近一周',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }, {
+                        text: '最近一个月',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }, {
+                        text: '最近三个月',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }]
+                },
+
             }
         },
         created() {
@@ -172,41 +207,43 @@
             handleCrane() {
                 this.distribution = true
             },
-            getTableData() {
-                getworkData(this.currentPage, this.$route.query.sn, this.pagesize).then(res => {
-                    console.log(2)
+            getTableData(time) {
+                console.log(time)
+                getworkData(this.currentPage, this.$route.query.sn, this.pagesize, time).then(res => {
                     if (res.data.response_status === "success") {
                         this.tableData = res.data.data.data
                         this.total = res.data.data.total
                     }
 
                     this.tableData.forEach(function (item, index) {
-                        item.errorMsg = item.a34001_Rtd_pre_warning ? '扬尘预警' : ''
-                        item.errorMsg += item.a34001_Rtd_is_warning ? '|扬尘报警' : ''
-                        item.errorMsg += item.a34002_Rtd_pre_warning ? '|PM10预警' : ''
-                        item.errorMsg += item.a34002_Rtd_is_warning ? '|PM10报警' : ''
-                        item.errorMsg += item.a34004_Rtd_pre_warning ? '|PM2.5预警' : ''
-                        item.errorMsg += item.a34004_Rtd_is_warning ? '|PM2.5报警' : ''
-                        item.errorMsg += item.LA_Rtd_pre_warning ? '|噪音上限预警' : ''
-                        item.errorMsg += item.LA_Rtd_is_warning ? '|噪音上限报警' : ''
-                        item.errorMsg += item.a01001_Rtd_high_pre_warning ? '|温度上限预警' : ''
-                        item.errorMsg += item.a01001_Rtd_high_is_warning ? '|温度上限报警' : ''
-                        item.errorMsg += item.a01001_Rtd_low_pre_warning ? '|温度下限预警' : ''
-                        item.errorMsg += item.a01001_Rtd_low_is_warning ? '|温度下限报警' : ''
-                        item.errorMsg += item.a01002_Rtd_pre_warning ? '|湿度上限预警' : ''
-                        item.errorMsg += item.a01002_Rtd_is_warning ? '|湿度上限报警' : ''
-                        item.errorMsg += item.a01006_Rtd_high_pre_warning ? '|气压上限预警' : ''
-                        item.errorMsg += item.a01006_Rtd_low_pre_warning ? '|气压下限预警' : ''
-                        item.errorMsg += item.a01006_Rtd_high_is_warning ? '|气压下限预警' : ''
-                        item.errorMsg += item.a01006_Rtd_low_is_warning ? '|气压下限报警' : ''
-                        item.errorMsg += item.a01007_Rtd_pre_warning ? '|风速上限预警' : ''
-                        item.errorMsg += item.a01007_Rtd_is_warning ? '|风速上限报警' : ''
+                        item.errorMsg = item.a34001_Rtd_pre_warning ? '扬尘预警|' : ''
+                        item.errorMsg += item.a34001_Rtd_is_warning ? '扬尘报警|' : ''
+                        item.errorMsg += item.a34002_Rtd_pre_warning ? 'PM10预警|' : ''
+                        item.errorMsg += item.a34002_Rtd_is_warning ? 'PM10报警|' : ''
+                        item.errorMsg += item.a34004_Rtd_pre_warning ? 'PM2.5预警|' : ''
+                        item.errorMsg += item.a34004_Rtd_is_warning ? 'PM2.5报警|' : ''
+                        item.errorMsg += item.LA_Rtd_pre_warning ? '噪音上限预警|' : ''
+                        item.errorMsg += item.LA_Rtd_is_warning ? '噪音上限报警|' : ''
+                        item.errorMsg += item.a01001_Rtd_high_pre_warning ? '温度上限预警|' : ''
+                        item.errorMsg += item.a01001_Rtd_high_is_warning ? '温度上限报警|' : ''
+                        item.errorMsg += item.a01001_Rtd_low_pre_warning ? '温度下限预警|' : ''
+                        item.errorMsg += item.a01001_Rtd_low_is_warning ? '温度下限报警|' : ''
+                        item.errorMsg += item.a01002_Rtd_pre_warning ? '湿度上限预警|' : ''
+                        item.errorMsg += item.a01002_Rtd_is_warning ? '湿度上限报警|' : ''
+                        item.errorMsg += item.a01006_Rtd_high_pre_warning ? '气压上限预警|' : ''
+                        item.errorMsg += item.a01006_Rtd_low_pre_warning ? '气压下限预警|' : ''
+                        item.errorMsg += item.a01006_Rtd_high_is_warning ? '气压下限预警|' : ''
+                        item.errorMsg += item.a01006_Rtd_low_is_warning ? '气压下限报警|' : ''
+                        item.errorMsg += item.a01007_Rtd_pre_warning ? '风速上限预警|' : ''
+                        item.errorMsg += item.a01007_Rtd_is_warning ? '风速上限报警|' : ''
+                        item.errorMsg = item.errorMsg.substring(0, item.errorMsg.length - 1)
                     })
                 })
             },
             search() {
-                this.getTableData(this.form)
-            }
+                let time = this.form.time instanceof Array ? this.form.time.join(',') : this.form.time
+                this.getTableData(time)
+            },
         }
 
     }
